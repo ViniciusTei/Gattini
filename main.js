9094db2fa16b53ab9be138655e5b801939613178
ViniciusTei <41354297+ViniciusTei@users.noreply.github.com>
2018-10-05 09:59:27 -0300

    Mudanças na tela de login
    Criada uma child window para a tela de login que desabilita a janela de menu até a validação do usuário.

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ const electron = require("electron");
 const remote = electron.remote;
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const ipcMain = electron.ipcMain;
 
 let win;
+let loginWindow;
 
 function createWindow(){
   //Create a new instance of the object window
@@ -13,19 +15,39 @@ function createWindow(){
     frame: true
   });
   //Load html file with the design of the window
-  win.loadFile('src/login.html')
+  win.loadFile('src/menu.html')
 
-  win.webContents.openDevTools();
+  //win.webContents.openDevTools();
   //Set the object to null so it can be closed
   win.on('close', () => {
     win = null;
   } )
 
-  win.on('ready-to-show', () => {
-    win.maximize();
-    win.show();
+  //Create a child window for the login authentication
+  loginWindow = new BrowserWindow({
+    parent: win,
+    frame: false,
+    autoHideMenuBar: true,
+    width: 400,
+    height: 500,
+    show: false
+  })
+
+  loginWindow.loadFile('src/login.html');
+
+  loginWindow.on('ready-to-show', () => {
+    loginWindow.show();
   })
 }
+
+//Open the main window when the auth is correct
+ipcMain.on('verificacao-valida', (event, arg) => {
+  if(arg == 'arg'){
+    win.maximize();
+    win.show();
+    loginWindow.hide();
+  }
+})
 
 //The parameter ready means that all objects have been initialized
 app.on('ready', createWindow);
@@ -33,12 +55,12 @@ app.on('ready', createWindow);
 //Specific code for Mac
 app.on('window-all-closed', () => {
   if(process.platform !== 'darwin'){
-    app.quit()
+    app.quit();
   }
 } )
 
 app.on('activate', () => {
   if(win == null){
-    createWindow()
+    createWindow();
   }
 })
--- src/login.js
+++ src/login.js
@@ -1,33 +1,11 @@
 const electron = require('electron');
-const remote = electron.remote;
-const BrowserWindow = electron.remote.BrowserWindow;
+const ipcRenderer = electron.ipcRenderer;
 
 const btnLogin = document.getElementById('login');
 
 btnLogin.addEventListener('click', function(){
+  /* Aqui vai o código para verificação do usuário */
 
-    //armazena a janela de login para fechá-la mais tarde
-    var loginScreen = remote.getCurrentWindow();
-
-    let menuWin = new BrowserWindow({
-      autoHideMenuBar: true,
-      show: false,
-      frame: true
-    });
-    //Load html file with the design of the window
-    menuWin.loadFile('src/menu.html');
-
-    menuWin.openDevTools();
-
-    //Set the object to null so it can be closed
-    menuWin.on('close', () => {
-      menuWin = null;
-    } )
-
-    menuWin.on('ready-to-show', () => {
-      menuWin.maximize();
-      menuWin.show();
-      loginScreen.close();
-    })
-
+  //se autenticação for válida
+  ipcRenderer.sendSync('verificacao-valida', 'arg');
 })
